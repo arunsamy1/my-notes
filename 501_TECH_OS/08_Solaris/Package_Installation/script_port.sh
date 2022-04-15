f87a46a059a50ebcb4e635f076c8258758872a1c
ARUN RAMASAMY <arun.ramasamy@gmail.com>
2022-04-15 17:06:45 -0500

    Update script_port.sh

--- 501_TECH_OS/08_Solaris/Package_Installation/script_port.sh
+++ 501_TECH_OS/08_Solaris/Package_Installation/script_port.sh
@@ -1,11 +1,14 @@
 #!/bin/ksh
 line='---------------------------------------------'
+
 pids=$(/usr/bin/ps -ef -o pid=)
+
 if [ $# -eq 0 ]; then
 read ans?"Enter Port Number To Know The pid: "
 else
 ans=$1
 fi
+
 for f in $pids
 do
 /usr/proc/bin/pfiles $f 2>/dev/null | /usr/xpg4/bin/grep -q "port: $ans"
@@ -14,4 +17,5 @@ echo $line
 echo "Port: $ans is used by PID:\c"
 pargs -l $f
 fi
+
 done
